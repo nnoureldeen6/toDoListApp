c2c68f5f8b3b8538edc958810dfa529af7f9008b
Nour <nnour4906@gmail.com>
2023-07-18 11:04:30 -0500

    CompleteIterationZero

---
+++ toDoListApp/toDoList.swift
@@ -0,0 +1,20 @@
+//
+//  toDoList.swift
+//  toDoListApp
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import Foundation
+
+
+class ToDoItem {
+    var title = ""
+    
+    var isImportant = false
+
+    init(title: String, isImportant: Bool = false){
+        self.title = title
+        self.isImportant = isImportant
+    }
+}
